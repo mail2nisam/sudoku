8b9c51928a341b1259642ff4c6966f7d7e8ee931
Nisam <mail2nisam@gmail.com>
2019-01-19 15:42:33 +0530

    css & js changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -7,7 +6,7 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,7 +19,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,6 +27,77 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app'
-});
+// const app = new Vue({
+//     el: '#app'
+// });
+
+var Sudoku = {
+    init: function () {
+        this.isNumberAllowed();
+        this.autoResolve();
+        this.validatePuzzle();
+        this.currentMatrix();
+    },
+    currentMatrix: function () {
+        let matrix = [];
+        let innerMatrix = [];
+        for (let i = 0; i < 9; i++) {
+            innerMatrix = [];
+            for (let j = 0; j < 9; j++) {
+                let cellName = "input_" + i + j;
+                innerMatrix[j] = $("input[name='" + cellName + "']").val();
+            }
+            matrix[i] = innerMatrix
+        }
+        return matrix;
+    },
+    isNumberAllowed: function () {
+        $("#sudoku").on("blur", ".input-number", function (a) {
+            var input = a.target;
+            let number = $(input).val();
+            window.axios.post("/api/sudoku/check-possibility/" + number, {
+                row: $(input).data("row"),
+                column: $(input).data("column"),
+                matrix_id: $("#matrix-id").val()
+            }).then(response => {
+                if (!response.data.status) {
+                    $(input).addClass("not-allowed")
+                } else {
+                    $(input).removeClass("not-allowed")
+                }
+            });
+        })
+    },
+    autoResolve: function () {
+        $("#solve_puzzle").click(function (e) {
+            e.preventDefault();
+            window.axios.get("/api/sudoku/auto-resolve/" + $("#matrix-id").val()).then(response => {
+                let matrix = response.data;
+                for (let i = 0; i < matrix.length; i++) {
+                    let row = matrix[i];
+                    for (let j = 0; j < row.length; j++) {
+                        let cellValue = row[j];
+                        let cellName = "input_" + i + j;
+                        $("input[name='" + cellName + "']").val(cellValue).addClass("new-values");
+                    }
+                }
+            });
+        });
+    },
+    validatePuzzle: function () {
+        $("#validate_puzzle").click(function (e) {
+            e.preventDefault();
+            let currentMatrix = Sudoku.currentMatrix();
+            window.axios.post("/api/sudoku/validate/" + $("#matrix-id").val(), {
+                matrix: currentMatrix
+            }).then(response => {
+                if(response.data.status===true){
+                    $(".validation-alert").removeClass('invisible').removeClass("alert-danger").addClass("alert-success").html("Sudo is Valid");
+                }else{
+                    $(".validation-alert").removeClass('invisible').removeClass("alert-success").addClass("alert-danger").html("Sudo is not Valid");
+                }
+            });
+        });
+    }
+};
+Sudoku.init();
